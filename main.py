c428a088cf4774a4dbd35e24eb7c3b6610aac48e
OmniaBene <Omniabene2@mail.ru>
2022-04-21 23:58:41 +0300

    вывод начало

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 from telebot import TeleBot
 
+bot = TeleBot('5317464669:AAG5R6klT_cURjoo0wbbe07mGwtVBu66x6Y')
 
-TeleBot.send_message()
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    with open('./Wordss/words.txt') as inp:
+        massiv = inp.read().split()
+        for i in range(len(massiv)):
+            mess = f'{massiv[i]} {massiv[1]}'
+            bot.send_message(message.chat.id, mess )
+
+
+@bot.message_handler()
+def get_user_text(message):
+    bot.send_message(message.chat.id, message, parse_mode='html')
+
+bot.polling(none_stop=True)
