b210c0961fa7187e9d452c924103ffd150019cfb
OmniaBene <Omniabene2@mail.ru>
2022-04-22 02:59:31 +0300

    Merge remote-tracking branch 'origin/nikita-dev' into nikita-dev
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -2,18 +2,29 @@ from telebot import TeleBot
 
 bot = TeleBot('5317464669:AAG5R6klT_cURjoo0wbbe07mGwtVBu66x6Y')
 with open('./Wordss/words.txt', encoding='utf-8') as inp:
-    words = inp.readline().split()
+    words = inp.readlines()
 
-# with open('./Wordss/transcriptions.txt') as f:
-#     transcriptions = f.readline().split()
+with open('./Wordss/transcriptions.txt', encoding='utf-8') as inp:
+    transcriptions = inp.readlines()
 
 with open('./Wordss/translation.txt', encoding='utf-8') as inp:
-    translation = inp.read().split()
+    translation = inp.readlines()
 
-@bot.message_handler(commands=['start'])
-def start(message):
+
+# @bot.message_handler(commands=['start'])
+# def start(message):
+#     for i in range(len(words)):
+#         mess = f'{words[i]} {transcriptions[i]} {translation[i]}'
+#         bot.send_message(message.chat.id, mess)
+
+@bot.message_handler()
+def word(message):
+    if message.text == "words":
         for i in range(len(words)):
-            mess = f'{words[i]}  {translation[i]}'
+            mess = f'{words[i]} {transcriptions[i]} {translation[i]}'
             bot.send_message(message.chat.id, mess)
+    else:
+        bot.send_message(message.chat.id, "я тебя не понимать")
+
 
 bot.polling(none_stop=True)
