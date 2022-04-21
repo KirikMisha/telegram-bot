0ce4e0743013f769ad22f8ece35cc904b581d4bb
OmniaBene <Omniabene2@mail.ru>
2022-04-22 01:16:00 +0300

    Merge remote-tracking branch 'origin/nikita-dev' into nikita-dev
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from telebot import TeleBot
 
 bot = TeleBot('5317464669:AAG5R6klT_cURjoo0wbbe07mGwtVBu66x6Y')
+with open('./Wordss/words.txt', encoding='utf-8') as inp:
+    words = inp.readline().split()
 
+# with open('./Wordss/transcriptions.txt') as f:
+#     transcriptions = f.readline().split()
+
+with open('./Wordss/translation.txt', encoding='utf-8') as inp:
+    translation = inp.read().split()
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    with open('./Wordss/words.txt') as inp:
-        massiv = inp.read().split()
-        for i in range(len(massiv)):
-            mess = f'{massiv[i]} {massiv[1]}'
-            bot.send_message(message.chat.id, mess )
-
-
-
+        for i in range(len(words)):
+            mess = f'{words[i]}  {translation[i]}'
+            bot.send_message(message.chat.id, mess)
 
 bot.polling(none_stop=True)
