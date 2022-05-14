87f8bdf0b98d517390b990f729488fd33743cd63
OmniaBene <Omniabene2@mail.ru>
2022-05-14 13:44:51 +0300

    игра в слова без проверки

--- handlers/expanded_game.py
+++ handlers/expanded_game.py
@@ -3,22 +3,25 @@ from aiogram.dispatcher import FSMContext
 from aiogram import types
 import random
 
+
 # Развертка меню игр----------------------------------------------------------------------------------------------------
 async def game_menu(callback: types.CallbackQuery):
-    await bot.delete_message(callback.from_user.id,callback.message.message_id)
+    await bot.delete_message(callback.from_user.id, callback.message.message_id)
     item1 = types.InlineKeyboardButton('🐊 Crocodile', callback_data='crocodile')
     item2 = types.InlineKeyboardButton('Word Game', callback_data='word_game')
     item3 = types.InlineKeyboardButton('⬅ Back to menu', callback_data='back_menu')
     markup = types.InlineKeyboardMarkup(row_width=2)
     markup.add(item1, item2, item3)
     await callback.message.answer('описание раздела игр', reply_markup=markup)
-#-----------------------------------------------------------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------------------------------------------------------
 
 
 # Игра в слова----------------------------------------------------------------------------------------------------------
 async def word_game(callback: types.CallbackQuery):
     await bot.delete_message(callback.from_user.id, callback.message.message_id)
-    item1 = types.InlineKeyboardButton('Да, поехали', callback_data='start_words_game')
+    item1 = types.InlineKeyboardButton('Да, поехали', callback_data='first_word_user_words_game')
     item2 = types.InlineKeyboardButton('⬅ Back to menu', callback_data='back_menu')
     markup = types.InlineKeyboardMarkup(row_width=2)
     markup.add(item1, item2)
@@ -26,13 +29,13 @@ async def word_game(callback: types.CallbackQuery):
                                   'Ты согласен продолжить, или вернешься назад?', reply_markup=markup)
 
 
-async def start_words_game(callback: types.CallbackQuery):
+async def first_word_user_words_game(callback: types.CallbackQuery):
     await bot.send_message(callback.message.chat.id, 'Ну если ты согласен, тогда начинай. \n'
                                                      'p.s(просто напиши любое слово из английского языка)')
     await words_game.first_word_user.set()
 
 
-async def first_word_user_words_game(message: types.Message, state: FSMContext):
+async def first_word_bot_words_game(message: types.Message, state: FSMContext):
     first_letter = message.text[-1].upper()
     with open(f'./Game/Words_to_words_game/{first_letter}.txt', encoding='utf-8') as inp:
         letter = inp.readlines()
@@ -46,55 +49,39 @@ async def first_word_user_words_game(message: types.Message, state: FSMContext):
     await words_game.first_word_bot.set()
 
 
-async def first_word_bot_words_game(message: types.Message, state: FSMContext):
+async def main_words_game(message: types.Message, state: FSMContext):
     data = await state.get_data()
-    last_letter = data.get('first_word_user')
-    answer =message.text
-    if answer[0].upper() != last_letter.upper():
-        await bot.send_message(message.chat.id, f'Так не пойдет, твое слово должно начинатся на: <b><u>{last_letter}'
-                                                f'</u></b>', parse_mode='html')
+    last_letter_first = data.get('first_word_user')
+    if message.text[0].upper() != last_letter_first.upper():
+        await bot.send_message(message.chat.id, f'Так не пойдет, твое слово должно начинатся на: '
+                                                f'<b><u>{last_letter_first}</u></b>', parse_mode='html')
         await words_game.first_word_bot.set()
         return
-    await bot.send_message(message.chat.id, last_letter)
-    await state.finish()
+
+    with open(f'./Game/Words_to_words_game/{message.text[-1].upper()}.txt', encoding='utf-8') as inp:
+        letter = inp.readlines()
+    i = random.randint(1, len(letter))
+    last_letter = letter[i].split('\t')
+    last_letter[0] = last_letter[0][-1]
+    await bot.send_message(message.chat.id, letter[i])
+    await bot.send_message(message.chat.id, f'Твоя очередь, слово должно начинатся на <b><u>{last_letter[0]}</u></b>'
+                                            f'', parse_mode='html')
+    await state.update_data(first_word_user=last_letter[0])
+    await words_game.first_word_bot.set()
 
 
-
-
-
-    # # def game_word(message):
-    # #     str = message.text[-1].upper()
-    #     with open(f'./Game/Words_to_words_game/{str}.txt', encoding='utf-8') as inp:
-    #         letter = inp.readlines()
-    #     i = random.randint(1, len(letter))
-    #     msg = bot.send_message(message.chat.id, letter[i])
-    #     bot.register_next_step_handler(msg, game_word)
-    # #
-    # def word_game(message):
-    #     rmk = types.ReplyKeyboardMarkup()
-    #     rmk.add(types.KeyboardButton("Да"), types.KeyboardButton("Нет"))
-    #
-    #     msg = bot.send_message(message.chat.id, "это игра в слова, хочешь продолжить?", reply_markup=rmk)
-    #     bot.register_next_step_handler(msg, start_word_game)
-    #
-    # def start_word_game(message):
-    #     if message.text == "Нет":
-    #         Game(message)
-    #     elif message.text == "Да":
-    #         msg = bot.send_message(message.chat.id, "Тогда начинай, твой ход")
-    #         bot.register_next_step_handler(msg, game_word)
-    #     else:
-    #         bot.send_message(message.chat.id, 'Так не пойдет')
-#-----------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------
 
 
 # Развертка игры крокодила----------------------------------------------------------------------------------------------
 async def crocodile_game(callback: types.CallbackQuery):
     pass
-#-----------------------------------------------------------------------------------------------------------------------
 
 
-#Развертка граматики----------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------
+
+
+# Развертка граматики----------------------------------------------------------------------------------------------------
 #     # elif message.text == '📝 Grammar':
 #     #     item1 = types.KeyboardButton('🕰️ Time')
 #     #     item2 = types.KeyboardButton('👮‍♂ Rules')
@@ -137,9 +124,9 @@ async def crocodile_game(callback: types.CallbackQuery):
 
 
 def expanded_game_f(dp: Dispatcher):
-    dp.register_callback_query_handler(game_menu,text='game_menu')
+    dp.register_callback_query_handler(game_menu, text='game_menu')
     dp.register_callback_query_handler(word_game, text='word_game')
-    dp.register_callback_query_handler(start_words_game, text='start_words_game')
+    dp.register_callback_query_handler(first_word_user_words_game, text='first_word_user_words_game')
     dp.register_callback_query_handler(crocodile_game, text='crocodile')
-    dp.register_message_handler(first_word_user_words_game,state=words_game.first_word_user)
-    dp.register_message_handler(first_word_bot_words_game, state=words_game.first_word_bot)
+    dp.register_message_handler(first_word_bot_words_game, state=words_game.first_word_user)
+    dp.register_message_handler(main_words_game, state=words_game.first_word_bot)
