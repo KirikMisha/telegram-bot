a03c290f15c8aa30ed2127adfe9853b5d2fa0947
OmniaBene <Omniabene2@mail.ru>
2022-05-14 12:02:40 +0300

    мише

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,5 +2,5 @@ from handlers import comands
 from handlers import expanded_game
 from handlers import expanded_vocabluary
 from handlers import expanded_links
-# from handlers import expanded_grammar
+from handlers import expanded_grammar
 # from handlers import test_vocadluary
